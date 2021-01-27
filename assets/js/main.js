7123b24334785a23614ee4c0497ff61523fa62a8
SteffenKloempges <steffen.kloempges@gmail.com>
2021-01-27 15:36:17 +0100

    allDone

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function showInput(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    console.log(name, email, message)
+}
